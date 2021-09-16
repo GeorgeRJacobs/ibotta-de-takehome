ea744a50f1d1753b60e47e8ef0cc99505d976d82
george.jacobs <george.jacobs@ibotta.com>
2021-09-16 16:55:13 -0600

    updated with env

--- src/task_1/local/dl_local.sh
+++ src/task_1/local/dl_local.sh
@@ -8,7 +8,7 @@ source .env
 set +a
 
 echo "Creating local directory"
-mkdir -p data/takehome-raw/$(date +%Y-%m-%d)
+mkdir -p data/$S3_BUCKET_NAME-raw/$(date +%Y-%m-%d)
 
 echo "Downloading 311 Service Locally"
 curl "https://www.denvergov.org/media/gis/DataCatalog/311_service_data/csv/311_service_data_2015.csv" -o data/takehome-raw/$(date +%Y-%m-%d)/service_data.csv > setup.log
