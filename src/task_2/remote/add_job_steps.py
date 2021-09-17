c7d36c88d547539a1d6d8a7a75174de0b44fe98c
george.jacobs <george.jacobs@ibotta.com>
2021-09-17 12:54:25 -0600

    small fix

--- src/task_2/remote/add_job_steps.py
+++ src/task_2/remote/add_job_steps.py
@@ -26,7 +26,7 @@ def main():
 
     args = parse_args()
     params = get_parameters()
-    steps = get_steps(params)
+    steps = get_steps(params, args.job_type)
 
     add_job_flow_steps(cl_dict['ClusterId'], steps)
 
