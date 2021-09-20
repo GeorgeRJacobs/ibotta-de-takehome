a11e977b79e0e340946ff8a3175408ff968c143e
george.jacobs <george.jacobs@ibotta.com>
2021-09-20 09:42:25 -0600

    updated the teardown scripts

--- src/remote/task_3/setup/setup.sh
+++ src/remote/task_3/setup/setup.sh
@@ -3,8 +3,8 @@
 echo "Setting up Remote EMR Environment & Port Forwarding"
 zsh src/remote/task_3/setup/0_setup_key_pair.sh
 
-zsh src/remote/task_3/setup/1_start_emr_w_jupyter.sh
+echo "SparkMagic implementation"
+zsh src/remote/task_3/setup/1_setup_sparkmagic.sh
 
-zsh src/remote/task_3/setup/2_setup_sparkmagic.sh
-
-zsh src/remote/task_3/setup/3_enable_port_forwarding.sh
+echo "Port Forwarding"
+zsh src/remote/task_3/setup/2_enable_port_forwarding.sh
--- src/teardown.sh
+++ src/teardown.sh
@@ -16,10 +16,6 @@ echo "Tearing down EMR cluster"
 EMR_CLUSTER_ID=$(aws emr list-clusters --active --query 'Clusters[?Name==`sde-lambda-etl-cluster`].Id' --output text)
 aws emr terminate-clusters --cluster-ids $EMR_CLUSTER_ID >> tear_down.log
 
-echo "Waiting for Teardown of EMR"
-EMR_CLUSTER_ID=$(aws emr list-clusters --cluster-states STARTING --query 'Clusters[?Name==`detakehome`].Id' --output text)
-aws emr wait cluster-running --cluster-id $EMR_CLUSTER_ID
-
 echo "Deleting Key Pair"
 aws ec2 delete-key-pair --key-name DE_TAKEHOME_ANALYSIS
 
